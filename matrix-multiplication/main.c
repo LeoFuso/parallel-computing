539d29a1b1de2c9862f86226040a80e3cd437876
LeoFuso <leonardofusonuzzo@gmail.com>
2018-09-13 18:02:46 -0300

    matrix_calculation terminado

--- matrix-multiplication/main.c
+++ matrix-multiplication/main.c
@@ -6,195 +6,224 @@ int
 rand_lim(int, int);
 
 int **
-populate(unsigned int, unsigned int);
+populate(unsigned int);
 
 int **
-allocate(unsigned int, unsigned int);
+allocate(unsigned int);
 
-void
-print_matrix(unsigned int, unsigned int, int **, char *);
+unsigned int
+check_sum(unsigned int, int **);
 
 void
 cleanup(unsigned int, int **);
 
-int **
-benchmark(int **(*fn)(unsigned int, unsigned int, int **, int **, int **),
-          unsigned int,
+double
+benchmark(int **(*fn)(unsigned int, int **, int **, int **),
           unsigned int,
           int **,
           int **);
 
 int **
-calculate_simple(unsigned int, unsigned int, int **, int **, int **);
+calculate_simple(unsigned int, int **, int **, int **);
 
 int **
-calculate_swapped(unsigned int, unsigned int, int **, int **, int **);
+calculate_swapped(unsigned int, int **, int **, int **);
+
+int **
+calculate_in_blocks(unsigned int, int **, int **, int **);
+
+unsigned int
+min(unsigned int, unsigned int);
 
 int
 main(void)
 {
-    unsigned int n_line = 3;
-    unsigned int n_column = 3;
+	unsigned int size = 2048;
 
-    int **matrix_A = NULL;
-    int **matrix_B = NULL;
-    int **matrix_result = NULL;
+	int **matrix_A = NULL;
+	int **matrix_B = NULL;
 
-    int **
-    (*calculate_v1)(unsigned int, unsigned int, int **, int **, int **) = NULL;
+	int **
+	(*calculate_v1)(unsigned int, int **, int **, int **) = NULL;
 
-    int **
-    (*calculate_v2)(unsigned int, unsigned int, int **, int **, int **) = NULL;
+	int **
+	(*calculate_v2)(unsigned int, int **, int **, int **) = NULL;
 
-    int **
-    (*calculate_v3)(unsigned int, unsigned int, int **, int **, int **) = NULL;
+	int **
+	(*calculate_v3)(unsigned int, int **, int **, int **) = NULL;
 
-    matrix_A = populate(n_line, n_column);
-    matrix_B = populate(n_line, n_column);
+	matrix_A = populate(size);
+	matrix_B = populate(size);
 
-    calculate_v1 = calculate_simple;
-    calculate_v2 = calculate_swapped;
+	calculate_v1 = calculate_simple;
+	calculate_v2 = calculate_swapped;
+	calculate_v3 = calculate_in_blocks;
 
-    print_matrix(n_line, n_column, matrix_A, "MATRIX A");
-    print_matrix(n_line, n_column, matrix_B, "MATRIX B");
+	printf("CALCULATE CLASSIC\n");
+	benchmark(calculate_v1, size, matrix_A, matrix_B);
+	printf("CALCULATE REVERSE\n");
+	benchmark(calculate_v2, size, matrix_A, matrix_B);
+	printf("CALCULATE IN BLOCKS\n");
+	benchmark(calculate_v3, size, matrix_A, matrix_B);
 
-    benchmark(calculate_v1, n_line, n_column, matrix_A, matrix_B);
-    benchmark(calculate_v2, n_line, n_column, matrix_A, matrix_B);
+	cleanup(size, matrix_A);
+	cleanup(size, matrix_B);
 
-    cleanup(n_line, matrix_A);
-    cleanup(n_line, matrix_B);
-    cleanup(n_line, matrix_result);
-
-    return 0;
+	return 0;
 }
 
 int **
-calculate_simple(unsigned int n_line, unsigned int n_column, int **matrix_A, int **matrix_B, int **matrix_result)
+calculate_simple(unsigned int size, int **matrix_A, int **matrix_B, int **matrix_result)
 {
-    unsigned int i;
-    unsigned int j;
-    unsigned int k;
+	unsigned int i;
+	unsigned int j;
+	unsigned int k;
 
-    for (i = 0; i < n_line; i++)
-        for (j = 0; j < n_line; j++)
-            for (k = 0; k < n_line; k++)
-                matrix_result[i][j] += matrix_A[i][k] * matrix_B[k][j];
+	for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
+			for (k = 0; k < size; k++)
+				matrix_result[i][j] += matrix_A[i][k] * matrix_B[k][j];
 
-    return matrix_result;
+	return matrix_result;
 }
 
 int **
-calculate_swapped(unsigned int n_line, unsigned int n_column, int **matrix_A, int **matrix_B, int **matrix_result)
+calculate_swapped(unsigned int size, int **matrix_A, int **matrix_B, int **matrix_result)
 {
-    unsigned int i;
-    unsigned int j;
-    unsigned int k;
+	unsigned int i;
+	unsigned int j;
+	unsigned int k;
 
-    for (i = 0; i < n_line; i++)
-        for (k = 0; k < n_line; k++)
-            for (j = 0; j < n_line; j++)
-                matrix_result[i][j] += matrix_A[i][k] * matrix_B[k][j];
-    return matrix_result;
+	for (i = 0; i < size; i++)
+		for (k = 0; k < size; k++)
+			for (j = 0; j < size; j++)
+				matrix_result[i][j] += matrix_A[i][k] * matrix_B[k][j];
+	return matrix_result;
 }
 
 int **
-benchmark(int **(*fn)(unsigned int, unsigned int, int **, int **, int **),
-          unsigned int n_line,
-          unsigned int n_column,
+calculate_in_blocks(unsigned int size, int **matrix_A, int **matrix_B, int **matrix_result)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int k;
+
+	unsigned int jj;
+	unsigned int kk;
+
+	unsigned int block_size = 256;
+
+	for (k = 0; k < size; k += block_size)
+		for (j = 0; j < size; j += block_size)
+			for (i = 0; i < size; ++i)
+				for (jj = j; jj < min(j + block_size, size); ++jj)
+					for (kk = k; kk < min(k + block_size, size); ++kk)
+						matrix_result[i][jj] += matrix_A[i][kk] * matrix_B[kk][jj];
+
+	return matrix_result;
+}
+
+double
+benchmark(int **(*fn)(unsigned int, int **, int **, int **),
+          unsigned int size,
           int **matrix_A,
           int **matrix_B)
 {
-    int **matrix_result = NULL;
-    matrix_result = allocate(n_line, n_column);
+	int **matrix_result = NULL;
+	matrix_result = allocate(size);
 
-    struct timeval tv;
-    double start_t, end_t, runtime;
+	struct timeval tv;
+	double start_t, end_t, runtime;
 
-    gettimeofday(&tv, NULL);
-    start_t = (double) tv.tv_sec + (double) tv.tv_usec / 1000000.0;
+	gettimeofday(&tv, NULL);
+	start_t = (double) tv.tv_sec + (double) tv.tv_usec / 1000000.0;
 
-    matrix_result = fn(n_line, n_column, matrix_A, matrix_B, matrix_result);
+	matrix_result = fn(size, matrix_A, matrix_B, matrix_result);
 
-    gettimeofday(&tv, NULL);
-    end_t = (double) tv.tv_sec + (double) tv.tv_usec / 1000000.0;
-    runtime = end_t - start_t;
+	gettimeofday(&tv, NULL);
+	end_t = (double) tv.tv_sec + (double) tv.tv_usec / 1000000.0;
+	runtime = end_t - start_t;
 
-    printf("RUNTIME: %f seconds\n", runtime);
-    print_matrix(n_line, n_column, matrix_result, "RESULT");
+	printf("  -   RUNTIME: %f seconds\n", runtime);
 
-    return matrix_result;
+	printf("  -    RESULT: %u\n", check_sum(size, matrix_result));
+	cleanup(size, matrix_result);
+
+	return runtime;
+}
+unsigned int
+min(unsigned int a, unsigned int b)
+{
+	return a < b ? a : b;
 }
 
 int **
-populate(unsigned int n_line, unsigned int n_column)
+populate(unsigned int size)
 {
-    int **matrix = NULL;
+	int **matrix = NULL;
 
-    matrix = allocate(n_line, n_column);
+	matrix = allocate(size);
 
-    unsigned i;
-    unsigned j;
-    for (i = 0; i < n_line; ++i)
-        for (j = 0; j < n_column; ++j)
-            matrix[i][j] = rand_lim(2, 25);
+	unsigned i;
+	unsigned j;
+	for (i = 0; i < size; ++i)
+		for (j = 0; j < size; ++j)
+			matrix[i][j] = rand_lim(2, 25);
 
-    return matrix;
+	return matrix;
 }
+
 int **
-allocate(unsigned int n_line, unsigned int n_column)
+allocate(unsigned int size)
 {
-    int **matrix = NULL;
+	int **matrix = NULL;
 
-    matrix = (int **) calloc(n_line, sizeof(int **));
+	matrix = (int **) calloc(size, sizeof(int **));
 
-    unsigned i;
-    for (i = 0; i < n_line; ++i)
-        matrix[i] = (int *) calloc(n_column, sizeof(int *));
+	unsigned i;
+	for (i = 0; i < size; ++i)
+		matrix[i] = (int *) calloc(size, sizeof(int *));
 
-    return matrix;
+	return matrix;
 }
+
 /*
  * Returns an integer in the range [from, to).
  */
 int
 rand_lim(int from, int to)
 {
-    int divisor = RAND_MAX / (to + 1);
-    int toReturn;
+	int divisor = RAND_MAX / (to + 1);
+	int toReturn;
 
-    do
-    {
-        toReturn = rand() / divisor;
-    }
-    while (!(from <= toReturn && toReturn <= to));
+	do
+	{
+		toReturn = rand() / divisor;
+	}
+	while (!(from <= toReturn && toReturn <= to));
 
-    return toReturn;
+	return toReturn;
 }
-void
-print_matrix(unsigned int n_line, unsigned int n_column, int **matrix, char *title)
-{
-    if (n_line >= 10 || n_column >= 10)
-        return;
 
-    printf("\n\n-----------\n%s", title);
-    printf("\n\n");
-
-    unsigned int i;
-    for (i = 0; i < n_line; ++i)
-    {
-        unsigned int j;
-        for (j = 0; j < n_column; ++j)
-        {
-            printf("%d\t", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
 void
 cleanup(unsigned int n_line, int **matrix)
 {
-    unsigned int i;
-    for (i = 0; i < n_line; ++i)
-        free(matrix[i]);
-    free(matrix);
+	unsigned int i;
+	for (i = 0; i < n_line; ++i)
+		free(matrix[i]);
+	free(matrix);
+}
+
+unsigned int
+check_sum(unsigned int size, int **matrix)
+{
+	unsigned int i;
+	unsigned int j;
+
+	unsigned int result = 0;
+
+	for (i = 0; i < size; ++i)
+		for (j = 0; j < size; ++j)
+			result += matrix[i][j];
+	return result;
 }
